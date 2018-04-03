d3815d63808d0e012fa8c0bc34fa96f348755492
Zack Fravel <zpfravel@email.uark.edu>
2018-04-02 23:57:08 -0500

    minor fixes

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService){
+    private authenticationService: AuthenticationService) {
 
   }
 
