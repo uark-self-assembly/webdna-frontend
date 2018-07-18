dd088b99235709156c22c41b0b2bf6e26a5b0bf8
Jace McPherson <jace-mcpherson@att.net>
2018-07-18 11:23:40 -0500

    Visualizer updated to Material

--- src/app/views/visualizer/visualizer.component.ts
+++ src/app/views/visualizer/visualizer.component.ts
@@ -4,14 +4,15 @@ import { ProjectService } from '../../services/project/project.service';
 
 @Component({
   selector: 'visualizer-cmp',
-  templateUrl: './visualizer.component.html'
+  templateUrl: './visualizer.component.html',
+  styleUrls: ['./visualizer.component.css']
 })
 export class VisualizerComponent implements OnInit {
 
   private projects: Project[] = [];
-  private text = 'testing';
-  private loading = true;
   private selectedProject: Project;
+
+  private loading = true;
   private forcing = false;
 
   constructor(private projectService: ProjectService) {
@@ -22,49 +23,40 @@ export class VisualizerComponent implements OnInit {
     this.loadProjects();
   }
 
-  selectProject(project: Project) {
-    this.selectedProject = project;
-  }
-
   loadProjects() {
     this.projectService.getProjects().then(response => {
-      if (typeof response === 'string') {
-        console.log('error');
-      } else {
+      if (typeof response !== 'string') {
         this.projects = response;
         if (Array.isArray(this.projects) && this.projects.length > 0) {
           this.selectedProject = this.projects[0];
         }
       }
-    }, error => {
-      console.log(error);
     });
   }
 
   loadProject() {
     this.loading = true;
+
+    console.log(this.selectedProject.id);
+
     const projectId = this.selectedProject.id;
-    this.projectService.getCurrentOutput(projectId).then(response => {
-      if (typeof response !== 'string') {
-        if (response.running) {
-          this.forcing = true;
-          this.projectService.generateVisualization(projectId).then(_ => {
-            this.forcing = false;
-            this.showHTMOL();
-          }, _ => {
-            this.showHTMOL();
-          });
-        } else {
+
+    this.projectService.getProjectById(projectId).then(response => {
+      if (response.running) {
+        this.forcing = true;
+        this.projectService.generateVisualization(projectId).then(_ => {
+          this.forcing = false;
           this.showHTMOL();
-        }
+        }, _ => {
+          this.showHTMOL();
+        });
+      } else {
+        this.showHTMOL();
       }
-    }, error => {
-      console.log(error);
     });
   }
 
   showHTMOL() {
-    console.log(this.selectedProject);
     this.loading = false;
   }
 }
